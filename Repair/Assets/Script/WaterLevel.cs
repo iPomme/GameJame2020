06879d9efeb908a11dd393baf8da67744a94872d
Nicolas Jorand <nicolas.jorand@me.com>
2020-02-01 17:11:41 +0100

    add game logic

---
+++ Repair/Assets/Script/WaterLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class WaterLevel : MonoBehaviour
+{
+
+    
+    public GameStatus gs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gs.waterLevel = transform.position.y;
+        gs.headsetLevel = OVRManager.tracker.GetPose(0).position.y;
+    }
+}
--- Repair/Assets/ScriptableObjects/GameStatus.cs
+++ Repair/Assets/ScriptableObjects/GameStatus.cs
@@ -1,8 +1,52 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
 namespace DefaultNamespace
 {
-    [UnityEngine.CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
-    public class GameObject : UnityEngine.ScriptableObject
+    [UnityEngine.CreateAssetMenu(fileName = "GameStatus", menuName = "Repair/GameStatus", order = 0)]
+    public class GameStatus : UnityEngine.ScriptableObject
     {
-        
+        [Header("Global configuration")] 
+        public int waterLevelSpeed;
+        public int waterLevelCheckIntervalInSeconds;
+
+        public int FailureGeneratorIntervalInSecond;
+        public int NbOfHoles
+        {
+            get => holes.Length;
+            set { holes = new Hole[value]; }
+        }
+
+        [Header("Game Status")] public float headsetLevel;
+        public float waterLevel;
+        public bool gameover;
+
+        private Hole[] holes;
+    }
+
+    public struct Hole
+    {
+        public HoleStatus Status;
+        public HoleShape Shape;
+
+        public Hole(HoleStatus holeStatus, HoleShape holeShape)
+        {
+            this.Status = holeStatus;
+            this.Shape = holeShape;
+        }
+    }
+
+    public enum HoleStatus
+    {
+        FIXED,
+        BROKEN
+    }
+
+    public enum HoleShape
+    {
+        ONE,
+        TWO,
+        THRE
     }
 }
